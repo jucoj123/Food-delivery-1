fa3ea536a9c8fb3f56763adc2a280d9aa745c871
jucoj123 <43160394+jucoj123@users.noreply.github.com>
2022-11-29 16:52:09 +0900

    commit

--- front/src/main/java/fooddelivery/external/PaymentService.java
+++ front/src/main/java/fooddelivery/external/PaymentService.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "front", url = "${api.url.front}")
+@FeignClient(
+    name = "front",
+    url = "${api.url.front}",
+    fallback = PaymentServiceImpl.class
+)
 public interface PaymentService {
     @RequestMapping(method = RequestMethod.POST, path = "/payments")
     public void pay(@RequestBody Payment payment);
---
+++ front/src/main/java/fooddelivery/external/PaymentServiceImpl.java
@@ -0,0 +1,15 @@
+package fooddelivery.external;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    /**
+     * Fallback
+     */
+    public Payment getPayment(Long id) {
+        Payment payment = new Payment();
+        return payment;
+    }
+}
